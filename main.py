7aa8c5093407066bccc6ca12ae16608da90697b4
Mark McCaskill <mmccaskill99@gmail.com>
2018-11-26 05:16:55 -0600

    Final user-signup

---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, redirect, render_template
+import re
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    username = ''
+    email = ''
+    username_error = ''
+    password_error = ''
+    verify_password_error = ''
+    email_error = ''
+    title = 'New User Sign Up'
+
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+        verify_password = request.form['verify_password']
+        email = request.form['email']
+
+        for i in username:
+            if i.isspace():
+                username_error = 'Your username must not contain spaces.'
+                username = ''
+            else:
+                if (len(username) < 3) or (len(username) > 20):
+                    username_error = 'Username needs to be between 3-20 characters.'
+                    username = ''
+
+        if not username:
+            username_error = 'Not a valid username'
+            username = ''
+
+        for i in password:
+            if i.isspace():
+                password_error = 'Password must not contain spaces.'
+            else:
+                if (len(password) < 3) or (len(password) > 20):
+                    password_error = 'Password must be 3-20 characters and not contain spaces.'
+        if not len(password):
+            password_error = 'This is not a valid password'
+
+        if password != verify_password:
+            verify_password_error = 'Your passwords do not match.'
+
+        if (email != '') and (not re.match('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)):
+            email_error = 'This is not a valid email address.'
+            email = ''
+
+        if (not password_error) and (not username_error) and (not email_error) and (not verify_password_error):
+            return redirect('/confirmedpage?username={0}'.format(username))
+
+    return render_template('signup.html', title=title, username=username, email=email,
+                           username_error=username_error, password_error=password_error,
+                           verify_password_error=verify_password_error, email_error=email_error)
+
+@app.route('/confirmedpage')
+def confirmedpage():
+    title = "Welcome New User!"
+    username = request.args.get('username')
+    return render_template('welcome.html', title=title, username=username)
+
+if __name__ == '__main__':
+    app.run()
